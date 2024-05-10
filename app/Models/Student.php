7dc9bd429663fbd57593be338726af17c6efdf00
Alan Lima <alancarlosml@gmail.com>
2024-05-10 18:27:35 -0300

    Update Student.php

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,8 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Student extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'cpf',
+        'email',
+        'email_verified_at',
+        'password',
+        'birthdate',
+        'address',
+        'phone',
+        'profile_picture',
+        'gender',
+        'active',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'birthdate',
+        'email_verified_at',
+        'deleted_at',
+    ];
 }
